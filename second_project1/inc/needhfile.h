b9636d98868f49df289de078284c707ebbcaa7ca
hqs IT master <86848510+HQS2KEQIN@users.noreply.github.com>
2023-05-31 19:57:50 +0800

    Update needhfile.h

--- second_project1/inc/needhfile.h
+++ second_project1/inc/needhfile.h
@@ -1,9 +1,9 @@
 #ifndef _NEEDHFILE_
 #define _NEEDHFILE_
-//文件的复制
+//鏂囦欢澶嶅埗
 int copy_file(char *str1,char *str2);
 
-//目录的复制
+//鐩綍澶嶅埗
 int open_dir(char *src_dir,char *dest_dir,thread_pool *pool);
 
 #endif
